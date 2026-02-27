650bed5a1e250e0b878ff8307d2d827e2742eb7e
munhooz <munhoz99.br@gmail.com>
2026-02-27 04:11:10 -0300

    feat: Salvar dark ou light theme no localStorage

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme === "dark") {
+  document.documentElement.classList.add("dark");
+} else if (savedTheme === "light") {
+  document.documentElement.classList.remove("dark");
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
